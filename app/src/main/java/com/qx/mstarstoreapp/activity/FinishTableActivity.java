2a192110d925f569ccdd5d1c174f4748ff83c2fa
tianfeng94666 <tianfeng94666@gmail.com>
2017-03-06 12:38:11 +0800

    提交

---
+++ app/src/main/java/com/qx/mstarstoreapp/activity/FinishTableActivity.java
@@ -0,0 +1,14 @@
+package com.qx.mstarstoreapp.activity;
+
+import com.qx.mstarstoreapp.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/3/6 0006.
+ */
+
+public class FinishTableActivity extends BaseActivity {
+    @Override
+    public void loadNetData() {
+
+    }
+}
